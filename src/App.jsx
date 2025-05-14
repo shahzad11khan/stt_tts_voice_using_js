455d9d715c11dd61b8b3466b26435d2d380eda55
KashifKhalil986 <kashifkhalil720@gmail.com>
2025-05-14 17:39:11 +0500

    modified voice pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import VoicePage from './Pages/VoicePage';
-import ChatBot from './Pages/ChatBot';
+// import ChatBot from './Pages/ChatBot';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ChatBot />} />
-        <Route path="/voice-page" element={<VoicePage />} />
+        {/* <Route path="/" element={<ChatBot />} /> */}
+        <Route path="/" element={<VoicePage />} />
       </Routes>
     </Router>
   );
